6ce755247ef569225f09cdd2c2ca2d4f212c9bb7
Oleg Burov <oleg.burov@outlook.com>
2018-09-01 19:01:04 -0700

    Add Vue.js

--- src/TweetBox/wwwroot/js/site.js
+++ src/TweetBox/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
