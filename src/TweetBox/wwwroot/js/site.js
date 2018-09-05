de1fc1119f80c14d6b30eb76d25c7798243e0ed3
Oleg Burov <oleg.burov@outlook.com>
2018-09-05 09:46:21 -0700

    Implement feature 'Attach Photo'.

--- src/TweetBox/wwwroot/js/site.js
+++ src/TweetBox/wwwroot/js/site.js
@@ -8,7 +8,8 @@ const MAX_TWEET_LENGTH = 140;
 new Vue({
   el: '#twitterVue',
   data: {
-    tweet: ''
+    tweet: '',
+    photo: null
   },
   computed: {
     tweetIsOutOfRange: function () {
@@ -19,11 +20,31 @@ new Vue({
       return MAX_TWEET_LENGTH - this.tweet.length;
     },
     underTwentyMark: function () {
-      return this.charactersRemaining <= 20
-        && this.charactersRemaining > 10;
+      return this.charactersRemaining <= 20 && this.charactersRemaining > 10;
     },
     underTenMark: function () {
       return this.charactersRemaining <= 10;
+    },
+    photoHasBeenUploaded: function () {
+      return this.photo !== null;
+    }
+  },
+  methods: {
+    triggerFileUpload: function () {
+      this.$refs.photoUpload.click();
+    },
+    handlePhotoUpload: function (e) {
+      var self = this;
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        self.photo = (e.target.result);
+      };
+
+      reader.readAsDataURL(e.target.files[0]);
+    },
+    removePhoto: function () {
+      this.photo = null;
     }
   }
 });
