5a6d948c5e2459a4e734a04fde5abebdcfa56ab3
Oleg Burov <oleg.burov@outlook.com>
2018-09-05 10:06:36 -0700

    Extend to attach multiple photos.

--- src/TweetBox/wwwroot/js/site.js
+++ src/TweetBox/wwwroot/js/site.js
@@ -9,7 +9,7 @@ new Vue({
   el: '#twitterVue',
   data: {
     tweet: '',
-    photo: null
+    photos: []
   },
   computed: {
     tweetIsOutOfRange: function () {
@@ -26,7 +26,7 @@ new Vue({
       return this.charactersRemaining <= 10;
     },
     photoHasBeenUploaded: function () {
-      return this.photo !== null;
+      return this.photos.length > 0
     }
   },
   methods: {
@@ -35,16 +35,20 @@ new Vue({
     },
     handlePhotoUpload: function (e) {
       var self = this;
-      var reader = new FileReader();
+      var files = e.target.files;
 
-      reader.onload = function (e) {
-        self.photo = (e.target.result);
-      };
+      for (let i = 0; i < files.length; i++) {
+        let reader = new FileReader();
 
-      reader.readAsDataURL(e.target.files[0]);
+        reader.onloadend = function (evt) {
+          self.photos.push(evt.target.result);
+        };
+
+        reader.readAsDataURL(files[i]);
+      }
     },
-    removePhoto: function () {
-      this.photo = null;
+    removePhoto: function (index) {
+      this.photos.splice(index, 1);
     }
   }
 });
